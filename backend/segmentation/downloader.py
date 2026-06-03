5f6ea226ac16be7fcc019c9aae591420944303e1
gabin.clerbout <gabin.clerbout@epita.fr>
2026-06-03 20:24:47 +0200

    feat: segmentation Otsu

--- backend/segmentation/downloader.py
+++ backend/segmentation/downloader.py
@@ -11,23 +11,20 @@ class LidcIdriDownloader:
     dir_path : Path = ""
 
 
-    def __init__(self, dir_path : Path | str):
+    def __init__(self, dir_path : Path | str, patient_ids):
         if isinstance(dir_path, str):
             dir_path = Path(dir_path)
 
         self.dir_path = dir_path
+        self.patient_ids = patient_ids
 
     def get_patient_path(self, pid) -> str :
         return str(self.dir_path / str(pid))
 
-    def download(self, patient_ids : list[str]) -> None:
+    def download(self) -> None:
         """
         Retrieve DICOM from patient ids.
-        :param patient_ids:
-        :return:
         """
-        self.patient_ids = patient_ids
-
         # Download
         for pid in self.patient_ids:
             series = nbia.getSeries(collection="LIDC-IDRI", patientId=pid)
@@ -43,15 +40,16 @@ class LidcIdriDownloader:
             ct_uids = [s['SeriesInstanceUID'] for s in ct_series]
             nbia.downloadSeries(ct_uids, input_type="list", path=self.get_patient_path(pid))
 
-            # get files info
-            patient_path = self.get_patient_path(pid)
-            subdirs = {d: len(os.listdir(os.path.join(patient_path, d)))
-                       for d in os.listdir(patient_path)
-                       if os.path.isdir(os.path.join(patient_path, d))}
-
-            self.patients_files_info[pid] = subdirs
-
     def get_patient_files_info(self, pid):
+        # get files info
+        if self.patients_files_info == {}:
+            for pid in self.patient_ids:
+                patient_path = self.get_patient_path(pid)
+                subdirs = {d: len(os.listdir(os.path.join(patient_path, d)))
+                           for d in os.listdir(patient_path)
+                           if os.path.isdir(os.path.join(patient_path, d))}
+                self.patients_files_info[pid] = subdirs
+
         patient_path = self.get_patient_path(pid)
         for d, count in self.patients_files_info[pid].items():
             dcm_files = [f for f in os.listdir(os.path.join(patient_path, d)) if f.endswith('.dcm')]
